386367c02d0bf5a4dfa12f595aaf568892b8e951
RKECUYQamx <appTest2@wavemaker.com>
2017-11-24 06:13:47 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_JOZoYlaauh",
+  "homePage" : "Main",
+  "name" : "Auto_JOZoYlaauh",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
